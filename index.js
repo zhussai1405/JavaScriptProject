d7f04fac878e11a8034785817086deb8dc09823b
zhussai1 <zhussai1@gmail.com>
2024-03-03 21:21:54 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+let greeting = "Hello Faraz, this is your first day of programming. I hope you will enjoy it :)";
+
+console.log(greeting);
