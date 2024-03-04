f80859e9ff7b47412c158fe868990a1e44f3a277
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-03-04 08:38:51 -0800

    javascript changese

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log(`Hello Node.js v${process.versions.node}!`);
 let greeting = "Hello Faraz, this is your first day of programming. I hope you will enjoy it :)";
 
 console.log(greeting);
+
+console.log("Test message");
